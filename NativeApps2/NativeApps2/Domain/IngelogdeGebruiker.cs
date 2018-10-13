dcb7ed6f3c69be30f852401d93246b2135606b0a
ruvdveld <ruben.vandevelde@student.hogent.be>
2018-10-13 11:48:14 +0200

    Scherm login (particulier en zakelijk)

--- NativeApps2/NativeApps2/Domain/IngelogdeGebruiker.cs
+++ NativeApps2/NativeApps2/Domain/IngelogdeGebruiker.cs
@@ -13,6 +13,7 @@ namespace NativeApps2.Domain
         private ObservableCollection<Onderneming> bedrijven;
         #region properties
         public string _naam { get; set; }
+        public string _voornaam { get; set; }
         public string _gebruikersnaam { get; set; }
         public string _wachtwoord { get; set; }
         public string _mail { get; set; }
@@ -22,9 +23,10 @@ namespace NativeApps2.Domain
         {
             //constructor van gebruiker
         }
-        public IngelogdeGebruiker(string naam, string gebruikersnaam, string wachtwoord, string mail)
+        public IngelogdeGebruiker(string naam, string voornaam, string gebruikersnaam, string wachtwoord, string mail)
         {
             _naam = naam;
+            _voornaam = voornaam;
             _gebruikersnaam = gebruikersnaam;
             _wachtwoord = wachtwoord;
             _mail = mail;
