ebad81aa1dfecfe3da65cf483c9555b84fa2ad18
BavoJanssensTIN <bavo.janssens@student.hogent.be>
2018-12-05 11:44:20 +0100

    Voeg PromotieGegevens en OverzichtPromoties toe

--- NativeApps2/NativeApps2/xaml pages/PromotieGegevens.xaml.cs
+++ NativeApps2/NativeApps2/xaml pages/PromotieGegevens.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NativeApps2.Domain;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,7 +25,18 @@ namespace NativeApps2.xaml_pages
     {
         public PromotieGegevens()
         {
+            //Test-fase
             this.InitializeComponent();
+            Onderneming ikea = new Onderneming("Ikea", "Meubels", "Sweden", "Ma-Vrij 08u00-17u30 zat-zon 08u-21u00", "ikea.png");
+            Promotie promotie = new Promotie("Black Friday", "Kortingen tot 70 procent", new DateTime(2018, 1, 1), new DateTime(2018, 1, 1), ikea, "70% korting");
+            promotieNaam.DataContext = promotie;
+            promotieGrid.DataContext = promotie;
+            eigenaar.DataContext = ikea;
+        }
+
+        private void Onderneming_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            framePromotieGegevens.Navigate(typeof(OndernemingGegevens));
         }
     }
 }
